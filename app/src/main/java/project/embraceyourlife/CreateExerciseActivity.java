7595537d0ef9a4488c5ab2ef6d863e026440a669
PrincePelican <radek18_10@mailmix.pl>
2021-06-17 16:48:17 +0200

    poprawka funkcji

--- app/src/main/java/project/embraceyourlife/CreateExerciseActivity.java
+++ app/src/main/java/project/embraceyourlife/CreateExerciseActivity.java
@@ -14,6 +14,8 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import project.embraceyourlife.datatypes.CwiczenieINFO;
+
 public class CreateExerciseActivity extends AppCompatActivity {
 
     private EditText _activity_name;
@@ -65,11 +67,11 @@ public class CreateExerciseActivity extends AppCompatActivity {
     }
 
     public void gotoweButtonListener(View v){
-//        Database baza = new Database(this);
-//        baza.insertIntoCwiczenie(getActivityName(), getActivityCategory(), _powtorzenia, _obciazenia, _czas_trwania, _dystans);
-//
-//        Intent i = new Intent(this, GymActivity.class);
-//        startActivity(i);
+        CwiczenieINFO Cwiczenie = new CwiczenieINFO(getActivityName(), _powtorzenia, _obciazenia, _czas_trwania, _dystans);
+        Database.getInstance(this).insert(Cwiczenie);
+
+        Intent i = new Intent(this, GymActivity.class);
+        startActivity(i);
     }
 
     public String getActivityName(){
