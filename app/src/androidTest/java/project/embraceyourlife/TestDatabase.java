e86c3b20af1ee4b03a4cb8a491ed186622a7ecf5
TheOneWhoCanCode <tomek31-ziomal@wp.pl>
2021-06-18 16:38:41 +0200

    Dodanie testu dla dodawania i usuwania wydarzenia

--- app/src/androidTest/java/project/embraceyourlife/TestDatabase.java
+++ app/src/androidTest/java/project/embraceyourlife/TestDatabase.java
@@ -9,10 +9,15 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+
 import project.embraceyourlife.datatypes.CwiczenieINFO;
+import project.embraceyourlife.datatypes.Wydarzenie;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class TestDatabase {
@@ -42,7 +47,33 @@ public class TestDatabase {
     }
 
     @Test
-    public void insert_getWydarzenie_removeWydarzenie() {
+    public void insert_getWydarzenia_removeWydarzenie() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Database db = Database.getInstance(appContext);
 
+        String data = "12/04/2015 14:20";
+
+        Wydarzenie wydarzenie = new Wydarzenie("nazwa", "",
+                data, 90, "Przykładowe wydarzenie");
+
+        db.insert(wydarzenie);
+        assertTrue(findWydarzenieInList(wydarzenie, db.getWydarzenia(data)));
+
+        db.removeWydarzenie(wydarzenie);
+        assertFalse(findWydarzenieInList(wydarzenie, db.getWydarzenia(data)));
+    }
+
+    public boolean findWydarzenieInList(Wydarzenie wydarzenie, ArrayList<Wydarzenie> lista) {
+        for (Wydarzenie w : lista) {
+            if (w.getNazwa().equals(wydarzenie.getNazwa()) &&
+                w.getPowtarzalnosc().equals(wydarzenie.getPowtarzalnosc()) &&
+                w.getData().equals(wydarzenie.getData()) &&
+                w.getCzasTrwania() == wydarzenie.getCzasTrwania() &&
+                w.getOpis().equals(wydarzenie.getOpis())) {
+
+                return true;
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/project/embraceyourlife/Database.java
+++ app/src/main/java/project/embraceyourlife/Database.java
@@ -177,8 +177,10 @@ public class Database extends SQLiteOpenHelper {
     public void removeWydarzenie(Wydarzenie wydarzenie) {
         SQLiteDatabase db = getWritableDatabase();
 
-        String[] whereArgs ={wydarzenie.getNazwa(), wydarzenie.getData(), wydarzenie.getOpis()};
-        db.delete("Wydarzenia", "nazwa LIKE ? AND data LIKE ? AND opis LIKE ?", whereArgs);
+        String[] whereArgs ={wydarzenie.getNazwa(), wydarzenie.getPowtarzalnosc(),
+                             wydarzenie.getData(), wydarzenie.getOpis()};
+
+        db.delete("Wydarzenia", "nazwa LIKE ? AND powtarzalnosc LIKE ? AND data LIKE ? AND opis LIKE ?", whereArgs);
     }
 
 
